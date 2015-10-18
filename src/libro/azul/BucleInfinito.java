8da93a19f2320ee89bebfb4c1cf60c9bed161e83
Tessier <prof.carlos.tessier@gmail.com>
2015-10-18 18:47:45 +0200

    first commit

---
+++ src/libro/azul/BucleInfinito.java
@@ -0,0 +1,22 @@
+package libro.azul;
+
+/**
+ * Clase principal de la aplicación. Tiene un bucle infinito
+ * que no hace nada.
+ *  
+ * @author Pedro Pablo Gómez Martín
+ */
+public class BucleInfinito {
+
+	/**
+	 * Método llamado al lanzar la aplicación. Entra en un
+	 * bucle infinito y no hace nada.
+	 * 
+	 * @param args Argumentos recibidos en la línea de comandos.
+	 */
+	public static void main(String[] args) {
+		while(true)
+			;
+	} // main
+
+} // class BucleInfinito
